740859fd256343dcaca622054c02feb34935724e
knigam <kushal.nigam1@gmail.com>
2015-01-08 00:29:24 -0500

    added icon, updated to latest version of android studio, added navegation drawer, list index of pictures and display the one selected

--- app/src/main/java/com/keonasoft/photofeed/app/MainActivity.java
+++ app/src/main/java/com/keonasoft/photofeed/app/MainActivity.java
@@ -9,13 +9,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Base64;
 import android.util.JsonReader;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
@@ -27,6 +31,7 @@ import com.loopj.android.http.ResponseHandlerInterface;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,22 +43,56 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends Activity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
-    String mCurrentPhotoPath;
-    FloatingActionButton cameraBtn;
-    ImageView mImageView;
+    private String mCurrentPhotoPath;
+    private FloatingActionButton cameraBtn;
+    private ImageView mImageView;
+    private ArrayList<String> listItems;
+    private Map<Integer, String> urls;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         HttpHelper.getInstance().initialize(getApplicationContext());
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        updateDrawerListItems();
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                HttpHelper.getInstance().getClient().get(urls.get(position), new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(responseBody,0, responseBody.length);
+                        mImageView.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+                mDrawerList.setItemChecked(position, true);
+                setTitle(listItems.get(position));
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
+
         cameraBtn = (FloatingActionButton) findViewById(R.id.camera_button);
         mImageView = (ImageView) findViewById(R.id.thumbnail);
 
@@ -93,7 +132,7 @@ public class MainActivity extends Activity {
 
             //Todo: send picture to server
             final String URL = getString(R.string.conn) + getString(R.string.picture_create);
-            File image = new File(mCurrentPhotoPath);
+            final File image = new File(mCurrentPhotoPath);
             RequestParams params = new RequestParams();
             params.put("text", image.getName());
             try {
@@ -102,7 +141,6 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
             }
             final String[] IMAGEURL = new String[1];
-            HttpHelper.getInstance().getClient().setTimeout(600);
             HttpHelper.getInstance().getClient().post(URL, params, new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
@@ -121,6 +159,8 @@ public class MainActivity extends Activity {
                         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                             Bitmap bitmap = BitmapFactory.decodeByteArray(responseBody,0, responseBody.length);
                             mImageView.setImageBitmap(bitmap);
+                            setTitle(image.getName());
+                            updateDrawerListItems();
                         }
 
                         @Override
@@ -171,6 +211,33 @@ public class MainActivity extends Activity {
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void updateDrawerListItems() {
+        urls = new HashMap<Integer, String>();
+        listItems = new ArrayList<String>();
+        HttpHelper.getInstance().getClient().get("http://kushalnigam.tk:8080/pictures.json", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                JSONArray json;
+                try {
+                    json = new JSONArray(new String(responseBody));
+                    for (int i = 0; i < json.length(); i++){
+                        JSONObject j = json.getJSONObject(i);
+                        listItems.add(j.getString("text"));
+                        urls.put(i, getString(R.string.conn) + j.getString("original_url"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                mDrawerList.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, listItems));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
     }
 
     private String imageToBase64(String fileName) throws FileNotFoundException {
